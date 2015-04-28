a1846798ce17297d8500ca7fef5c2d339548f2d4
Bob Brumfield <bob.brumfield@gmail.com>
2015-04-28 13:14:20 -0700

    Add appServices project

---
+++ projects/appServices.js
@@ -0,0 +1,24 @@
+/*jslint node: true */
+
+var colors = require("colors"),
+    config = require("../config"),
+    console = require("../console"),
+
+var AppServicesProject = function () {
+    'use strict';
+    var self = this;
+    self.name = "App Services";
+    self.description = "App Services projects";
+    self.icon = "";
+    self.frequency = config.get('defaultFrequency');
+
+    self.hueGroupId = "4";
+    
+    self.getState = function() {
+    	return config.get("projectStates:success");
+    };
+
+    return self;
+};
+
+module.exports = new AppServicesProject();
